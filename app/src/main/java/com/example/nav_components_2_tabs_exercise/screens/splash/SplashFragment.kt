55c6394420709b2d95cca055413b9ee1a56b199e
gonemc <gonemc91@gmail.com>
2023-06-09 08:24:48 +0300

    First step nav components.

--- app/src/main/java/com/example/nav_components_2_tabs_exercise/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/example/nav_components_2_tabs_exercise/screens/splash/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nav_components_2_tabs_exercise.screens.splash
 
+import android.content.Intent
 import android.os.Build.VERSION_CODES.S
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment
 import com.example.nav_components_2_tabs_exercise.R
 import com.example.nav_components_2_tabs_exercise.Repositories
 import com.example.nav_components_2_tabs_exercise.databinding.FragmentSplashBinding
+import com.example.nav_components_2_tabs_exercise.screens.main.MainActivity
+import com.example.nav_components_2_tabs_exercise.screens.main.MainActivityArgs
 import com.example.nav_components_2_tabs_exercise.utils.observeEvent
 import com.example.nav_components_2_tabs_exercise.utils.viewModelCreator
 
@@ -27,7 +30,13 @@ class SplashFragment: Fragment(R.layout.fragment_splash) {
     }
 
     private fun launchMainScreen(isSignedIn: Boolean){
-        TODO("Launch MainActivity here and send isSignedIn flag to it")
+        val intent = Intent(requireContext(), MainActivity::class.java)
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+        val args = MainActivityArgs(isSignedIn)
+        intent.putExtras(args.toBundle())
+        startActivity(intent)
     }
 
     private fun renderAnimations(){
