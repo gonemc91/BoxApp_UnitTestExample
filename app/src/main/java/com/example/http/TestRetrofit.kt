ecfdbd1a0c4af158eccc13862000e1b5067406cd
gonemc <gonemc91@gmail.com>
2023-10-19 19:25:51 +0300

    Test Retrofit

---
+++ app/src/main/java/com/example/http/TestRetrofit.kt
@@ -0,0 +1,64 @@
+package com.example.http
+
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+data class SignInRequestBody( //data class for Request
+    val email: String,
+    val password: String
+)
+
+data class SignInResponseBody( //data class for Response
+    val token:String
+)
+
+interface Api{
+    //Retrofit Function
+    @POST("sign-in") //annotation POST
+    suspend fun signIn(@Body body: SignInRequestBody): SignInResponseBody //annotation Body suspend request
+}
+
+
+fun main() = runBlocking{
+    //1) create interceptor
+    val loggingInterceptor = HttpLoggingInterceptor() // logger for request
+        .setLevel(HttpLoggingInterceptor.Level.BODY) //level logging MAX
+//2)create client
+    val client = OkHttpClient.Builder() //create client
+        .addInterceptor(loggingInterceptor) //add interceptor
+        .build()
+
+    //3) create moshi for parsing data
+val moshi = Moshi.Builder().build()
+
+    //4)create adapter for parsing json request and json format response
+    val moshiConverterFactory =
+        MoshiConverterFactory.create(moshi)
+
+    //5) create retrofit
+    val retrofit = Retrofit.Builder()
+        .baseUrl("http://127.0.0.1:12345")
+        .client(client)
+        .addConverterFactory(moshiConverterFactory)
+        .build()
+
+    //using
+    val api = retrofit.create(Api::class.java)  //realization interface Api with instruction for task (sign-in)
+
+    val requestBody = SignInRequestBody( //init request
+        email = "admin@google.com",
+        password = "123"
+    )
+
+    val response  = api.signIn(requestBody)
+
+
+    println("TOKEN: ${response.token}")
+}
